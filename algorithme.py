a1c96af927cb3acec234d80a35b3c67d013381d2
melcoloy <melcoloy@gmail.com>
2026-03-17 13:56:02 +0100

    allez la

--- algorithme.py
+++ algorithme.py
@@ -12,35 +12,58 @@ def generer_inventaire(type_jeu="double_six", nb_boites=1):
     jeu_complet = boite_unique * nb_boites
     return jeu_complet
 
-def a_voisin_libre(r, c, couvert, lignes, colonnes):
-    """Vérifie si la case (r,c) a au moins un voisin direct non couvert."""
-    for vr, vc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
-        if 0 <= vr < lignes and 0 <= vc < colonnes and not couvert[vr, vc]:
-            return True
-    return False
-
-def cree_isole(i1, j1, i2, j2, couvert, lignes, colonnes):
-    """Simule la pose pour vérifier si cela crée un trou (pixel isolé) autour."""
+def danger_composante_impaire(i1, j1, i2, j2, couvert, lignes, colonnes):
+    """
+    Simule la pose d'un domino et utilise un BFS (Parcours en largeur) 
+    pour vérifier si on ne crée pas une zone vide de taille IMPAIRE 
+    (qui serait mathématiquement impossible à remplir).
+    """
+    # 1. On simule la pose
     couvert[i1, j1] = True
     couvert[i2, j2] = True
-    isole = False
     
+    # On crée une copie de la grille pour le scan
+    visite = np.copy(couvert)
+    danger = False
+    
+    # 2. On regarde les cases autour du domino qu'on vient de poser
     voisins = [(i1-1, j1), (i1+1, j1), (i1, j1-1), (i1, j1+1),
                (i2-1, j2), (i2+1, j2), (i2, j2-1), (i2, j2+1)]
                
     for r, c in voisins:
-        if 0 <= r < lignes and 0 <= c < colonnes and not couvert[r, c]:
-            if not a_voisin_libre(r, c, couvert, lignes, colonnes):
-                isole = True
+        if 0 <= r < lignes and 0 <= c < colonnes and not visite[r, c]:
+            # --- DÉBUT DU BFS (Scan de la zone vide) ---
+            taille_zone = 0
+            file = [(r, c)]
+            visite[r, c] = True
+            
+            idx = 0
+            while idx < len(file):
+                curr_r, curr_c = file[idx]
+                idx += 1
+                taille_zone += 1
+                
+                # On regarde les 4 directions
+                for vr, vc in [(curr_r-1, curr_c), (curr_r+1, curr_c), (curr_r, curr_c-1), (curr_r, curr_c+1)]:
+                    if 0 <= vr < lignes and 0 <= vc < colonnes and not visite[vr, vc]:
+                        visite[vr, vc] = True
+                        file.append((vr, vc))
+            # --- FIN DU BFS ---
+            
+            # Si la zone vide qu'on a isolée a une taille impaire = Trous garantis plus tard !
+            if taille_zone % 2 != 0:
+                danger = True
                 break
                 
+    # 3. On annule la pose simulée
     couvert[i1, j1] = False
     couvert[i2, j2] = False
-    return isole
+    
+    return danger
 
 def placer_dominos(matrice_valeurs, stock_dominos):
     """
-    Algorithme Glouton avec Anticipation (Lookahead 1 coup)
+    Algorithme Glouton avec Anticipation Topologique (BFS)
     """
     lignes, colonnes = matrice_valeurs.shape
     couvert = np.zeros((lignes, colonnes), dtype=bool)
@@ -57,9 +80,9 @@ def placer_dominos(matrice_valeurs, stock_dominos):
             peut_H = (j + 1 < colonnes) and not couvert[i, j + 1]
             peut_V = (i + 1 < lignes) and not couvert[i + 1, j]
             
-            # --- LE MÉCANISME D'ANTICIPATION ---
-            danger_H = cree_isole(i, j, i, j+1, couvert, lignes, colonnes) if peut_H else True
-            danger_V = cree_isole(i, j, i+1, j, couvert, lignes, colonnes) if peut_V else True
+            # --- LE MÉCANISME D'ANTICIPATION PUISSANT ---
+            danger_H = danger_composante_impaire(i, j, i, j+1, couvert, lignes, colonnes) if peut_H else True
+            danger_V = danger_composante_impaire(i, j, i+1, j, couvert, lignes, colonnes) if peut_V else True
             
             meilleur_ecart_H, meilleur_idx_H, inv_H = 999, -1, False
             meilleur_ecart_V, meilleur_idx_V, inv_V = 999, -1, False
@@ -87,21 +110,22 @@ def placer_dominos(matrice_valeurs, stock_dominos):
                         meilleur_idx_V = index
                         inv_V = (err_V_inv < err_V_norm)
 
-            # --- LE CHOIX INTELLIGENT ---
+            # --- LE CHOIX DE L'ORIENTATION ---
             if peut_H and peut_V:
-                # La sécurité d'abord : on évite les choix mortels
+                # Priorité absolue à la sécurité géométrique
                 if danger_H and not danger_V:
                     choix = 'V'
                 elif danger_V and not danger_H:
                     choix = 'H'
                 else:
-                    # Si tout est sûr, on prend le plus beau visuellement
+                    # Si aucun danger, on prend la meilleure orientation pour l'image
                     choix = 'H' if meilleur_ecart_H <= meilleur_ecart_V else 'V'
             elif peut_H:
                 choix = 'H'
             elif peut_V:
                 choix = 'V'
             else:
+                # Si on est vraiment coincé, on ignore la case (rare avec ce nouvel algorithme)
                 continue
                 
             if choix == 'H':
